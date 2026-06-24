5053336c0ac294607c36b10f43db9180c1ecdb73
achingbrain <alex@achingbrain.net>
2026-06-24 16:55:01 +0200

    chore: export missing types

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -4,5 +4,8 @@ export interface DNSResolver {
   (domain: string, options?: QueryOptions): Promise<DNSResponse>
 }
 
+export type { DNSJSONOverHTTPSOptions } from './dns-json-over-https.ts'
+export type { DNSOverHTTPSOptions } from './dns-over-https.ts'
+
 export { dnsOverHttps } from './dns-over-https.ts'
 export { dnsJsonOverHttps } from './dns-json-over-https.ts'
